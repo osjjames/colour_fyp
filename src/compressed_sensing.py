e9827e478b518c045c6f7a5ae6dbfe27b630bc76
Ollie James <osj.james@gmail.com>
2020-03-05 12:29:47 +0000

    Create demo script

--- src/compressed_sensing.py
+++ src/compressed_sensing.py
@@ -5,8 +5,10 @@ import scipy.optimize as spopt
 import scipy.fftpack as spfft
 import scipy.ndimage as spimg
 import cvxpy as cvx
+from tqdm import tqdm
 from pylbfgs import owlqn
 import zhang
+
 
 def dct2(x):
     return spfft.dct(spfft.dct(x.T, norm='ortho', axis=0).T, norm='ortho', axis=0)
@@ -50,54 +52,33 @@ def evaluate(x, g, step):
 def progress(x, g, fx, xnorm, gnorm, step, k, ls):
   """Just display the current iteration.
   """
-  print('Iteration {}'.format(k))
+  if k % 10 == 0:
+    print('Calculating... (iteration {})'.format(k))
   return 0
 
 
 
 path = '/src/zhang/demo/imgs/rooster.jpg'
-sample_scale = 0.1
-zoom_scale = 1
+sample_scale = 0.2
+zoom_scale = 0.5
 
 # read original image and downsize for speed
 Xorig = spimg.imread(path, flatten=True, mode='L') # read in grayscale
 X = spimg.zoom(Xorig, zoom_scale)
-# plt.imshow(X, cmap='gray')
-print('X')
-print(X.shape)
+plt.imshow(X, cmap='gray')
+plt.show()
 ny,nx = X.shape
 
 # extract small sample of signal
 k = round(nx * ny * sample_scale) # 50% sample
 if k < 1:
   print('Sample size too small')
+else:
+  print('Sample ratio: ' + str(sample_scale*100) + '% of original')
 
 ri = np.random.choice(nx * ny, k, replace=False) # random sample of indices
 b = X.T.flat[ri]
 b = np.expand_dims(b, axis=1)
-
-print('b')
-print(b.shape)
-
-# # create dct matrix operator using kron (memory errors for large ny*nx)
-# A = np.kron(
-#     spfft.idct(np.identity(nx), norm='ortho', axis=0),
-#     spfft.idct(np.identity(ny), norm='ortho', axis=0)
-#     )
-# A = A[ri,:] # same as phi times kron
-# print('A')
-# print(A.shape)
-
-# # do L1 optimization
-# vx = cvx.Variable((nx * ny, 1))
-# print('vx')
-# print(vx.shape)
-# objective = cvx.Minimize(cvx.norm(vx, 1))
-
-# constraints = [A*vx == b]
-# prob = cvx.Problem(objective, constraints)
-# result = prob.solve(verbose=True)
-# Xat2 = np.array(vx.value).squeeze()
 
 Xat2 = owlqn(nx*ny, evaluate, progress, 5)
 
@@ -118,6 +99,6 @@ Xm.T.flat[ri] = X.T.flat[ri]
 plt.imshow(Xa, cmap='gray')
 plt.show()
 
-Xa_col = zhang.colorize_from_grayscale(Xa)
-plt.imshow(Xa_col)
-plt.show()
+# Xa_col = zhang.colorize_from_grayscale(Xa)
+# plt.imshow(Xa_col)
+# plt.show()
--- src/histogram_test.py
+++ src/histogram_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from tqdm import tqdm
+from tqdm import tqdm_notebook
 
 def compare_frames(frame1, frame2, binCount = 16):
   assert frame1.shape == frame2.shape, "Frames must be of equal dimensions"
@@ -13,30 +13,44 @@ def compare_frames(frame1, frame2, binCount = 16):
 
   diff = 0
   for i in range(binCount):
-    diff += abs(im1[i] - im2[i])  # Calculate difference between each pair of historgram bins
+    diff += abs(im1[i] - im2[i])  # Calculate difference between each pair of histogram bins
 
   similarity = 1 - (diff/(dimX*dimY)) # Similarity as a percentage
   return similarity
 
 
+def split_video(path):
+  frames = []
+  vid = cv2.VideoCapture(path)  # Import video
+  success,frame = vid.read()   # Read first frame
+  while success:
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert to grayscale
+    frames.append(grayFrame)
+    success,frame = vid.read()  # Read next frame
+  if len(frames) == 0:
+    print('Could not load video')
 
-frames = []
-vid = cv2.VideoCapture('/src/vid5.mp4')  # Import video
-success,frame = vid.read()   # Read first frame
-while success:
-  grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert to grayscale
-  frames.append(grayFrame)
-  success,frame = vid.read()  # Read next frame
+  groups = []
+  groups.append([])
+  curr_group = 0
+  for x in tqdm_notebook(range(len(frames) - 2)):
+    similarity = compare_frames(frames[x], frames[x+1])
+    groups[curr_group].append(frames[x])
 
-for x in tqdm(range(len(frames) - 1)):
-  similarity = compare_frames(frames[x], frames[x+1])
+    if similarity < 0.5:
+      fig = plt.figure()
+      ax1 = fig.add_subplot(1,2,1)
+      ax1.imshow(frames[x])
+      ax2 = fig.add_subplot(1,2,2)
+      ax2.imshow(frames[x+1])
+      plt.show()
+      print('Similarity: ' + str(similarity))
+      groups.append([])
+      curr_group += 1
 
-  if similarity < 0.6:
-    plt.imshow(frames[x])
-    plt.show()
-    plt.imshow(frames[x+1])
-    plt.show()
-    print(similarity)
+  groups[curr_group].append(frames[len(frames) - 1])
+  return groups
+
 
 
 
